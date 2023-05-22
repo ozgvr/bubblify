c7342a88211cbe6e6e1e775663fd40c78f28cbb7
Özgür Akın <ozgurakn14@gmail.com>
2023-05-23 02:40:10 +0300

    add parse artists function

--- js/app.js
+++ js/app.js
@@ -16,8 +16,28 @@ async function getTopArtists(access_token) {
         return response.json();
     })
     .then(data=>{
-        console.log(data.items)
+        console.log(parseArtists(data))
     })
     .catch(error => {
     });
 }
+
+function parseArtists(data) {
+    var artists = {};
+    var results = [];
+  
+    data.items.forEach(item => {
+      var artist = item.artists[0].name;
+      if (artists.hasOwnProperty(artist)) {
+        artists[artist]++;
+      } else {
+        artists[artist] = 1;
+      }
+    });
+  
+    for (let artist in artists) {
+      results.push({ name: artist, value: artists[artist] });
+    }
+  
+    return results;
+  }
