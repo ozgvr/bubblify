636da4ed50cf128f2690ea5a2e995e3d9e70af00
Özgür Akın <ozgurakn14@gmail.com>
2023-05-23 04:47:24 +0300

    create seperate functions for artists and tracks

--- js/app.js
+++ js/app.js
@@ -3,6 +3,33 @@ async function getTopArtists(access_token) {
     const offset = 0;
     const time_range = "medium_term";
     
+    fetch('https://api.spotify.com/v1/me/top/artists?limit='+limit+'&offset='+offset+'&time_range='+time_range, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            logout();
+        }
+        return response.json();
+    })
+    .then(data=>{
+        const parsedArtists = parseArtists(data);
+        console.log(parsedArtists)
+        renderBubbleChart(parsedArtists);
+
+    })
+    .catch(error => {
+    });
+}
+
+async function getTopTracks(access_token) {
+    const limit = 50;
+    const offset = 0;
+    const time_range = "medium_term";
+    
     fetch('https://api.spotify.com/v1/me/top/tracks?limit='+limit+'&offset='+offset+'&time_range='+time_range, {
         method: 'GET',
         headers: {
